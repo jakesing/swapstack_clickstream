378c87841c3f372bea2aab57a8b9dd919cf62a86
Jude Fernandes <jude.manutd@gmail.com>
2023-03-03 15:34:01 +0530

    added pipeline

--- api/src/functions/sync/index.ts
+++ api/src/functions/sync/index.ts
@@ -3,18 +3,18 @@ import { handlerPath } from "@libs/handler-resolver";
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   events: [
-    {
+    /* {
       schedule: {
         rate: ["rate(6 hours)"],
         enabled: true,
         name: "${env:ENVIRONMENT}_CalendlyAvailabilitySync",
       },
-    },
-    /* {
+    }, */
+    {
       http: {
         method: "get",
         path: "sync",
       },
-    }, */
+    },
   ],
 };
