671d1111f769b9502c7a509089c59b75dfa5067a
Jude Fernandes <jude.manutd@gmail.com>
2023-03-04 23:21:41 +0530

    updated serverless queue

--- api/src/functions/process/index.ts
+++ api/src/functions/process/index.ts
@@ -5,7 +5,7 @@ export default {
   events: [
     {
       sqs: {
-        arn: "arn:aws:sqs:${opt:region}:${aws:accountId}:${env:ENVIRONMENT}_click_event_s3",
+        arn: "arn:aws:sqs:${opt:region}:${aws:accountId}:${env:ENVIRONMENT}_click_event_s3_queue",
         // TODO: update count of messages handled by the lambda
         batchSize: 10,
       },
