6ffa5232bf3a94756ed5ba61e1750a3912051972
Jude Fernandes <jude.manutd@gmail.com>
2023-03-08 00:45:35 +0530

    dropped stage resources [ci skip]

--- infra/stage/main.tf
+++ infra/stage/main.tf
@@ -35,39 +35,39 @@ resource "aws_s3_bucket_versioning" "tfstate_bucket_version" {
 }
 
 # SQS Policy
-data "aws_iam_policy_document" "queue" {
-  #  Allow S3 to notify the SQS queue about an event
-  statement {
-    effect = "Allow"
+# data "aws_iam_policy_document" "queue" {
+#   #  Allow S3 to notify the SQS queue about an event
+#   statement {
+#     effect = "Allow"
 
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
+#     principals {
+#       type        = "*"
+#       identifiers = ["*"]
+#     }
 
-    actions   = ["sqs:SendMessage"]
-    resources = ["arn:aws:sqs:*:*:${var.environment}_click_event_s3_queue"]
+#     actions   = ["sqs:SendMessage"]
+#     resources = ["arn:aws:sqs:*:*:${var.environment}_click_event_s3_queue"]
 
-    condition {
-      test     = "ArnEquals"
-      variable = "aws:SourceArn"
-      values   = ["arn:aws:s3:::${var.click_event_bucket_name}"]
-    }
-  }
-}
+#     condition {
+#       test     = "ArnEquals"
+#       variable = "aws:SourceArn"
+#       values   = ["arn:aws:s3:::${var.click_event_bucket_name}"]
+#     }
+#   }
+# }
 
-resource "aws_sqs_queue" "queue" {
-  name                       = "${var.environment}_click_event_s3_queue"
-  policy                     = data.aws_iam_policy_document.queue.json
-  visibility_timeout_seconds = 120
-  receive_wait_time_seconds  = 20
-}
+# resource "aws_sqs_queue" "queue" {
+#   name                       = "${var.environment}_click_event_s3_queue"
+#   policy                     = data.aws_iam_policy_document.queue.json
+#   visibility_timeout_seconds = 120
+#   receive_wait_time_seconds  = 20
+# }
 
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = var.click_event_bucket_name
+# resource "aws_s3_bucket_notification" "bucket_notification" {
+#   bucket = var.click_event_bucket_name
 
-  queue {
-    queue_arn = aws_sqs_queue.queue.arn
-    events    = ["s3:ObjectCreated:*"]
-  }
-}
+#   queue {
+#     queue_arn = aws_sqs_queue.queue.arn
+#     events    = ["s3:ObjectCreated:*"]
+#   }
+# }
