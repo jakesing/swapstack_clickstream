25d80995acc007eafdc5643f6ee95df7eedd7ad3
Jude Fernandes <jude.manutd@gmail.com>
2023-03-09 12:22:30 +0530

    lambda based memory size optimization

--- api/src/functions/analytics/index.ts
+++ api/src/functions/analytics/index.ts
@@ -2,13 +2,13 @@ import { handlerPath } from "@libs/handler-resolver";
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
+  memorySize: 1024,
   events: [
     {
       http: {
         method: "get",
         path: "analytics",
         cors: true,
-        memorySize: 1024,
         // private: true,
       },
     },
